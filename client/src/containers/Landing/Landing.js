b8cac515feeec16f5258c776ca42f2c25830766f
MacGee20 <giftomodu20@gmail.com>
2019-10-17 23:32:26 +0100

    Merge branch 'develop' of https://github.com/hngi/nodejs-update into develop

--- client/src/containers/Landing/Landing.js
+++ client/src/containers/Landing/Landing.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Landing.css';
 import GoogleAuth from '../../components/GoogleAuth/GoogleAuth';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { GoogleLogout } from 'react-google-login';
 import { logout } from '../../actions/auth';
-const Landing = ({ isSignedInWithGoogle, logout }) => {
+import { upload } from '../../actions/upload';
+const Landing = ({ isSignedInWithGoogle, logout, upload, uploadstate }) => {
+  const [formData, setFormData] = useState({
+    name: '',
+    to: '',
+    link: '',
+    file: ''
+  });
+  const { name, to, link, file } = formData;
+  const onChange = e => {
+    setFormData({
+      ...formData,
+      [e.target.name]:
+        e.target.name !== 'file' ? e.target.value : e.target.files[0]
+    });
+    console.log(formData);
+  };
+  console.log(uploadstate);
   return (
     <div>
       <header>
@@ -38,50 +55,62 @@ const Landing = ({ isSignedInWithGoogle, logout }) => {
             <button
               id='btn-email'
               className='btn btn-primary'
-              onClick={() => {
-                document
-                  .getElementsByClassName('form2')[0]
-                  .classList.add('d-none');
-                var form1 = document.getElementsByClassName('form1')[0];
-                form1.classList.contains('d-none')
-                  ? form1.classList.remove('d-none')
-                  : form1.classList.add('d-none');
-              }}>
+              // onClick={() => {
+              //   document
+              //     .getElementsByClassName('form2')[0]
+              //     .classList.add('d-none');
+              //   var form1 = document.getElementsByClassName('form1')[0];
+              //   form1.classList.contains('d-none')
+              //     ? form1.classList.remove('d-none')
+              //     : form1.classList.add('d-none');
+              // }}
+              >
               Upload by email
             </button>
             <button
               id='btn-link'
               className='btn btn-primary ml-5'
-              onClick={() => {
-                document
-                  .getElementsByClassName('form1')[0]
-                  .classList.add('d-none');
-                var form2 = document.getElementsByClassName('form2')[0];
-                form2.classList.contains('d-none')
-                  ? form2.classList.remove('d-none')
-                  : form2.classList.add('d-none');
-              }}>
-              Upload by link
+              // onClick={() => {
+              //   document
+              //     .getElementsByClassName('form1')[0]
+              //     .classList.add('d-none');
+              //   var form2 = document.getElementsByClassName('form2')[0];
+              //   form2.classList.contains('d-none')
+              //     ? form2.classList.remove('d-none')
+              //     : form2.classList.add('d-none');
+              // }}
+              >
+              Copy link
             </button>
             {''}
-            <div className='form1 d-none'>
-              <form action>
+            <div className='form1'>
+              <form
+                onSubmit={e => {
+                  e.preventDefault();
+                  upload(name,to,file,true)
+                }}>
                 <div className='form-group'>
-                  <label htmlFor='email'>Email</label>
+                  <label htmlFor='name'>Name</label>
                   <input
                     className='form-control'
-                    type='email'
-                    placeholder='Your email address'
-                    id='email'
+                    type='name'
+                    placeholder='Your name'
+                    id='name'
+                    name='name'
+                    value={name}
+                    onChange={e => onChange(e)}
                   />
-                  <label htmlFor='Remail'>Reciever Email</label>
+                  <label htmlFor='Remail'>Receiver's Email</label>
                   <input
                     className='form-control'
                     type='email'
-                    placeholder='email address of Reciever'
+                    placeholder='Email of receiver'
                     id='Remail'
+                    name='to'
+                    value={to}
+                    onChange={e => onChange(e)}
                   />
-                  <label htmlFor='email'>Message</label>
+                  {/* <label htmlFor='email'>Optional message</label>
                   <textarea
                     className='form-control'
                     name
@@ -89,9 +118,15 @@ const Landing = ({ isSignedInWithGoogle, logout }) => {
                     cols={10}
                     rows={3}
                     defaultValue={''}
+                  /> */}
+                  <label htmlFor='file'>Upload file</label>
+                  <input
+                    name='file'
+                    //value={file}
+                    onChange={e => onChange(e)}
+                    className='form-control-file'
+                    type='file'
                   />
-                  <label htmlFor>Upload file</label>
-                  <input className='form-control-file' type='file' />
                   <input
                     type='submit'
                     className='btn btn-primary float-right'
@@ -104,9 +139,14 @@ const Landing = ({ isSignedInWithGoogle, logout }) => {
               </form>
             </div>
             <div className='form2 d-none'>
-              <form action>
+              <form
+                onSubmit={e => {
+                  e.preventDefault();
+                  upload(link,file,
+                    false)
+                }}>
                 <div className='form-group'>
-                  <label htmlFor='email'>Message</label>
+                  {/* <label htmlFor='email'>Message</label>
                   <textarea
                     className='form-control'
                     name
@@ -114,10 +154,13 @@ const Landing = ({ isSignedInWithGoogle, logout }) => {
                     cols={10}
                     rows={3}
                     defaultValue={''}
-                  />
-                  <label htmlFor>Upload file</label>
+                  /> */}
+                  <label htmlFor='file '>Upload file</label>
                   <input className='form-control-file' type='file' />
                   <input
+                    name='file'
+                    value={file}
+                    onChange={e => onChange(e)}
                     type='submit'
                     className='btn btn-primary float-right'
                     defaultValue='Generate Link'
@@ -143,9 +186,10 @@ const Landing = ({ isSignedInWithGoogle, logout }) => {
   );
 };
 const mapStateToProps = state => ({
-  isSignedInWithGoogle: state.auth.isSignedInWithGoogle
+  isSignedInWithGoogle: state.auth.isSignedInWithGoogle,
+  uploadstate: state.upload
 });
 export default connect(
   mapStateToProps,
-  { logout }
+  { logout, upload }
 )(Landing);
