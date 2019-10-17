b16a24a52545b64f36044ff3c874c5e43221e154
MacGee20 <giftomodu20@gmail.com>
2019-10-17 22:57:01 +0100

    Unique Id Field

--- server/models/ShortenLink.js
+++ server/models/ShortenLink.js
@@ -3,11 +3,12 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const urlShortenSchema = new Schema({
-  cloudinaryUrl: String,
-  shortUrlParam: String,
-  shortUrl: String,
+  _id: mongoose.Schema.Types.ObjectId,
+  cloudinaryUrl: String ,
+  shortUrlParam: String ,
+  shortUrl: String ,
   createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now }
+  updatedAt: { type: Date, default: Date.now },
 });
 
 const shortenLink = mongoose.model('ShortLink', urlShortenSchema);
